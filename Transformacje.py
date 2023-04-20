b900ea4228f8daaa99e0938c31e46e33e67a2630
Grabarzd <domino.s2@outlook.com>
2023-04-20 12:43:38 +0200

    Przeliczenie współrzędnych płaskich na fi, lam, h
    utworzenie funkcji na przeliczenie danych wyjściowych

--- Transformacje.py
+++ Transformacje.py
@@ -17,3 +17,23 @@ class Transformacje():
 
         self.flattening = (self.a - self.b) / self.a
         self.e2 = (2 * self.flattening - self.flattening ** 2)
+        
+        
+        def hirvonen(self,X,Y,Z):
+            p = np.sqrt(X**2 + Y**2)
+            #print('p=',p)
+            f = np.arctan(Z /( p * (1 - self.e2)))
+            #dms(f)
+            while True:
+                N = self.a / np.sqrt(1- self.e2 * np.sin(f)**2)
+                #print('N = ',N)
+                h = (p / np.cos(f)) - N
+               # print('h = ',h)
+                fp = f
+                f = np.arctan(Z / (p * (1 - self.e2 * (N / (N + h)))))
+                #dms(f)
+                if np.abs(fp - f) <( 0.000001/206265):
+                    break
+            l = np.arctan2(Y,X)
+            print(f,l,h)
+            return (f,l,h)
